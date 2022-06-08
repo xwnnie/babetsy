44c5bbf263715423768ca8e506e65b3e3077abd8
xwnnie <niexiaowen90@gmail.com>
2022-06-07 18:58:33 -0700

    order state

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,43 +1,56 @@
-import React, { useState, useEffect } from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import { useDispatch, useSelector } from 'react-redux';
+import React, { useState, useEffect } from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
 
 import { authenticate } from "./store/session";
 import { loadProducts } from "./store/products";
 
-import CategoryNav from './components/CategoryNav';
-import MyNav from './components/MyNav';
-import HomePage from './components/HomePage';
-import Products from './components/Products';
-import SingleProduct from './components/SingleProduct';
-import Cart from './components/Cart';
+import CategoryNav from "./components/CategoryNav";
+import MyNav from "./components/MyNav";
+import HomePage from "./components/HomePage";
+import Products from "./components/Products";
+import SingleProduct from "./components/SingleProduct";
+import Cart from "./components/Cart";
 
-import LoginForm from './components/auth/LoginForm';
-import SignUpForm from './components/auth/SignUpForm';
-import NavBar from './components/AuthNav';
-import ProtectedRoute from './components/auth/ProtectedRoute';
-import UsersList from './components/UsersList';
-import User from './components/User';
-
+import LoginForm from "./components/auth/LoginForm";
+import SignUpForm from "./components/auth/SignUpForm";
+import NavBar from "./components/AuthNav";
+import ProtectedRoute from "./components/auth/ProtectedRoute";
+import UsersList from "./components/UsersList";
+import User from "./components/User";
 
 function App() {
   const [loaded, setLoaded] = useState(false);
   const dispatch = useDispatch();
 
-  const sessionUser = useSelector(state => state.session.user);
+  const sessionUser = useSelector((state) => state.session.user);
 
   useEffect(() => {
-    (async() => {
+    (async () => {
       await dispatch(authenticate());
       setLoaded(true);
     })();
   }, [dispatch]);
 
-    useEffect(() => {
-      (async () => {
-        dispatch(loadProducts());
-      })();
-    }, [dispatch]);
+  useEffect(() => {
+    (async () => {
+      dispatch(loadProducts());
+    })();
+  }, [dispatch]);
+
+  useEffect(() => {
+    (async () => {
+      if (sessionUser) {
+        const res = await fetch(`/api/users/${sessionUser.id}`);
+        if (res.ok) {
+          const data = await res.json();
+          // dispatch(setOrders(data.orders));
+          // dispatch(setFavorites(data.favorite_products));
+        }
+      }
+      setLoaded(true);
+    })();
+  }, [dispatch, sessionUser]);
 
   if (!loaded) {
     return null;
