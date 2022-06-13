6c3ae1d631c24f1ca566e2cfbd0bace6c7d34645
xwnnie <niexiaowen90@gmail.com>
2022-06-12 23:05:25 -0700

    email validation

--- app/forms/signup_form.py
+++ app/forms/signup_form.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, IntegerField
+from wtforms import StringField
 from wtforms.validators import DataRequired, Email, ValidationError
 from app.models import User
 
@@ -33,7 +33,7 @@ def phone_validation(form, field):
 class SignUpForm(FlaskForm):
     username = StringField(
         'username', validators=[DataRequired(), username_exists])
-    email = StringField('email', validators=[DataRequired(), user_exists])
+    email = StringField('email', validators=[DataRequired(), Email(), user_exists])
     password = StringField('password', validators=[DataRequired()])
     address = StringField('address', validators=[DataRequired()])
     phone = StringField('phone', validators=[DataRequired(), phone_validation])
