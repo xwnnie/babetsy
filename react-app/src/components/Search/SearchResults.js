6d9d62694f33cf33ec890ac720e7a033acc8bb50
xwnnie <niexiaowen90@gmail.com>
2022-06-14 13:39:48 -0700

    search styling

--- react-app/src/components/Search/SearchResults.js
+++ react-app/src/components/Search/SearchResults.js
@@ -21,33 +21,37 @@ const SearchResult = () => {
   };
 
   return (
-    <div className="main-container">
-      <div id="search-h2">
-        Showing Results for
-        <span> "{searchQuery}" </span>
+    <div className="summer-container">
+      <div className="summer-header search-results"></div>
+      <div className="main-container">
+        <div id="search-h2">
+          Showing Results for
+          <span> "{searchQuery}" </span>
+        </div>
+        {!results.length ? (
+          <div className="no-search-div">
+            <div id="no-search-msg">NO MATCHING ITEMS</div>
+          </div>
+        ) : (
+          <div className="products-container">
+            {results.map((product) => (
+              <div key={product.id} className="product-card">
+                <FaveHeart productId={product.id} />
+                <img
+                  src={product.image_url}
+                  alt={product.name}
+                  onClick={(e) => handleOnClick(product.id)}
+                />
+                <div>${product.price}</div>
+                <div className="products-page-name">{product.name}</div>
+              </div>
+            ))}
+          </div>
+        )}
       </div>
-      {!results.length ? (
-        <div className="no-search-div">
-          <div id="no-search-msg">NO MATCHING ITEMS</div>
-        </div>
-      ) : (
-        <div className="products-container">
-          {results.map((product) => (
-            <div key={product.id} className="product-card">
-              <FaveHeart productId={product.id} />
-              <img
-                src={product.image_url}
-                alt={product.name}
-                onClick={(e) => handleOnClick(product.id)}
-              />
-              <div>${product.price}</div>
-              <div className="products-page-name">{product.name}</div>
-            </div>
-          ))}
-        </div>
-      )}
     </div>
   );
+  
 };
 
 export default SearchResult;
